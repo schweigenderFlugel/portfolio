936dcc00c99bd84250acdd7d159383400ab816c3
schweigenderFlugel <facundoezcastro@email.com>
2024-05-27 16:08:07 -0300

    resolve the problems with the carousel on the projects card and add the praesentation card

---
+++ src/const/badges.ts
@@ -0,0 +1,70 @@
+import Express from "../icons/Express.astro"
+import Nestjs from "../icons/Nestjs.astro"
+import React from "../icons/React.astro"
+import Postgresql from "../icons/Postgresql.astro"
+import Tailwind from "../icons/Tailwind.astro"
+import Typescript from "../icons/Typescript.astro"
+import Javascript from "../icons/Javascript.astro"
+import Php from "../icons/Php.astro"
+import Laravel from "../icons/Laravel.astro"
+import Sequelize from "../icons/Sequelize.astro"
+import Typeorm from "../icons/Typeorm.astro"
+
+export const badges = [
+  {
+    name: "Javascript",
+    class: "w-3 h-3",
+    icon: Javascript,
+  },
+  {
+    name: "Typescript",
+    class: "w-3 h-3",
+    icon: Typescript,
+  },
+  {
+    name: "PHP",
+    class: "w-3 h-3",
+    icon: Php,
+  },
+  {
+    name: "Express",
+    class: "w-3 h-3",
+    icon: Express,
+  },
+  {
+    name: "Nestjs",
+    class: "w-3 h-3",
+    icon: Nestjs,
+  },
+  {
+    name: "Laravel",
+    class: "w-3 h-3",
+    icon: Laravel,
+  },
+  {
+    name: "React",
+    class: "w-3 h-3",
+    icon: React,
+  },
+  {
+    name: "Sequelize",
+    class: "w-3 h-3",
+    icon: Sequelize,
+  },
+  {
+    name: "Typeorm",
+    class: "w-3 h-3",
+    icon: Typeorm,
+  },
+  {
+    name: "Postgresql",
+    class: "w-3 h-3",
+    icon: Postgresql,
+  },
+  {
+    name: "Tailwind",
+    class: "w-3 h-3",
+    icon: Tailwind,
+  }
+]
+  
