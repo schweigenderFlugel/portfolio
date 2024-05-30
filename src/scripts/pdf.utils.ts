14ae9df6e56da2d7d93a0f4659fa501116a38ca2
schweigenderFlugel <facundoezcastro@email.com>
2024-05-30 20:18:27 -0300

    design responsive, starting to work with the pdf document and erase projects page

--- src/scripts/pdf.utils.ts
+++ src/scripts/pdf.utils.ts
@@ -1,28 +1,79 @@
-import { PDFDocument, rgb } from 'pdf-lib';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 
-export async function generatePdf() {
+interface IAcademic {
+  title: string;
+  start: number;
+  finished: number | string;
+  college: string;
+}
 
-  console.log('You should see this message when you click this button')
+export interface IResume {
+  fullName: string;
+  dni: number;
+  cuil: number;
+  country: string;
+  state: string;
+  address: string;
+  phone: number;
+  email: string;
+  presentation: string;
+  academic: IAcademic[];
+  additional?: {
+    languages: string[];
+  }
+  laboral?: string[];
+}
+
+export async function generatePdf(resume: IResume) {
+  const data = [
+    resume.fullName,
+    resume.dni.toString(),
+    resume.cuil.toString(),
+    resume.country,
+    resume.state,
+    resume.address,
+    resume.phone.toString(),
+    resume.email,
+  ]
+
+  const properties = [
+    'Nombre', 
+    'DNI', 
+    'CUIL', 
+    'País', 
+    'Provincia/Estado', 
+    'Dirección', 
+    'Teléfono', 
+    'Correo'
+  ]
 
   const pdfDoc = await PDFDocument.create();
-  const page = pdfDoc.addPage([600, 400]);
+  const page = pdfDoc.addPage([595.28, 841.89]);
   const { width, height } = page.getSize();
-  const fontSize = 30;
+  const fontSize = 14;
+  const marginY = 50;
+  const marginX = 50;
+  let yPosition = height - marginY - fontSize;
 
-  // Agregar texto al PDF
-  page.drawText('Hello, world!', {
-    x: 50,
-    y: height - 4 * fontSize,
-    size: fontSize,
-    color: rgb(0, 0.53, 0.71),
-  });
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  const timesRomanFontBold = await pdfDoc.embedFont(StandardFonts.TimesRomanBold);
+
+  data.forEach((text, index) => {
+    page.drawText(text, {
+      x: marginX,
+      y: yPosition,
+      size: fontSize,
+      font: timesRomanFont,
+      color: rgb(0, 0, 0),
+    });
+    yPosition -= fontSize + 5;
+  })
+  
 
   const pdfBytes = await pdfDoc.save();
 
   const blob = new Blob([pdfBytes], { type: 'application/pdf' });
   const url = URL.createObjectURL(blob);
-
-  console.log(url)
 
   return url;
 }
