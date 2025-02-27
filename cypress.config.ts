f58806d749c964c4c6f0736595034071d5363838
schweigenderFlugel <facundoezcastro@gmail.com>
2025-02-26 23:03:57 -0300

    add docker and python icons, and more projects

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'cypress'
-import fs from 'fs'
+import fs from 'node:fs';
 
 export default defineConfig({
   e2e: {
     supportFile: false,
     baseUrl: "http://localhost:9000",
+    chromeWebSecurity: false,
     video: true,
     videoCompression: true,
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, _config) {
       on(
         'after:spec',
-        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+        (_spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
           if(results && results.video) {
             const failures = results.tests.some(test => 
               test.attempts.some(attempt => attempt.state === 'failed')
